8b912a9bb472a04849dd61960ac83cd3e5a02496
mrsheepuk <mark@mrsheep.co.uk>
2015-10-19 15:24:13 +0100

    Tweaks to the RSA key utils.

--- src/TokenAuthExampleWebApplication/RSAKeyUtils.cs
+++ src/TokenAuthExampleWebApplication/RSAKeyUtils.cs
@@ -25,20 +25,12 @@ namespace TokenAuthExampleWebApplication
             }
         }
 
-        /// <summary>
-        /// Util class to allow restoring RSA parameters from JSON as the normal
-        /// RSA parameters class won't restore private key info.
-        /// </summary>
-        private class RSAParametersWithPrivate
+        public static void GenerateKeyAndSave(string file)
         {
-            public byte[] D { get; set; }
-            public byte[] DP { get; set; }
-            public byte[] DQ { get; set; }
-            public byte[] Exponent { get; set; }
-            public byte[] InverseQ { get; set; }
-            public byte[] Modulus { get; set; }
-            public byte[] P { get; set; }
-            public byte[] Q { get; set; }
+            var p = GetRandomKey();
+            RSAParametersWithPrivate t = new RSAParametersWithPrivate();
+            t.SetParameters(p);
+            File.WriteAllText(file, JsonConvert.SerializeObject(t));
         }
 
         /// <summary>
@@ -62,17 +54,51 @@ namespace TokenAuthExampleWebApplication
         {
             if (!File.Exists(file)) throw new FileNotFoundException("Check configuration - cannot find auth key file: " + file);
             var keyParams = JsonConvert.DeserializeObject<RSAParametersWithPrivate>(File.ReadAllText(file));
-            return new RSAParameters()
+            return keyParams.ToRSAParameters();
+        }
+
+
+        /// <summary>
+        /// Util class to allow restoring RSA parameters from JSON as the normal
+        /// RSA parameters class won't restore private key info.
+        /// </summary>
+        private class RSAParametersWithPrivate
+        {
+            public byte[] D { get; set; }
+            public byte[] DP { get; set; }
+            public byte[] DQ { get; set; }
+            public byte[] Exponent { get; set; }
+            public byte[] InverseQ { get; set; }
+            public byte[] Modulus { get; set; }
+            public byte[] P { get; set; }
+            public byte[] Q { get; set; }
+
+            public void SetParameters(RSAParameters p)
             {
-                D = keyParams.D,
-                DP = keyParams.DP,
-                DQ = keyParams.DQ,
-                Exponent = keyParams.Exponent,
-                InverseQ = keyParams.InverseQ,
-                Modulus = keyParams.Modulus,
-                P = keyParams.P,
-                Q = keyParams.Q
-            };
+                D = p.D;
+                DP = p.DP;
+                DQ = p.DQ;
+                Exponent = p.Exponent;
+                InverseQ = p.InverseQ;
+                Modulus = p.Modulus;
+                P = p.P;
+                Q = p.Q;
+            }
+            public RSAParameters ToRSAParameters()
+            {
+                return new RSAParameters()
+                {
+                    D = this.D,
+                    DP = this.DP,
+                    DQ = this.DQ,
+                    Exponent = this.Exponent,
+                    InverseQ = this.InverseQ,
+                    Modulus = this.Modulus,
+                    P = this.P,
+                    Q = this.Q
+
+                };
+            }
         }
     }
 }
